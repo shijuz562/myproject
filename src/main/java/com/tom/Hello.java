7377f1b530d70fa1e3464b9536ac2f35cd3882d4
shijuz562 <shijuzh562>
2021-03-03 14:38:18 +0800

    commit message

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,9 @@
+package com.tom;
+
+public class Hello{
+    public static void main(String[] arg){
+//        System.out.println("Hello Java and Kotlin");
+        Person p = new Person();
+        p.hello();
+    }
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main(args: Array<String>){
+//    println("Hello Kotlin and Java")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin class")
+    }
+}
