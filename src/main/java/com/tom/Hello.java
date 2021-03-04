2abdc9084f3fdb02c5741145bbc7b94eca44e229
Alan <shijuz562@gmail.com>
2021-03-04 14:25:56 +0800

    Java and Kotlin calss field

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -1,32 +1,63 @@
 package com.tom;
 
+import com.sun.image.codec.jpeg.TruncatedFileException;
+
+import java.util.Locale;
+
 public class Hello{
     public static void main(String[] arg){
 //        System.out.println("Hello Java and Kotlin");
-//        new Person().hello();
-//        Person p = new Person();
-//        p.hello();
-        int age = 23;
-        Integer age2 = age;
-        byte agebyte = age2.byteValue();
-        System.out.println("My age byte is "+ agebyte);
+        new Person().hello();
+        Person p = new Person();
+        p.name = "沈俊璋";
+        p.weight = 68.5f;
+        p.height = 1.68f;
+        System.out.println(p.bmi());
 
-        char gender = 'a' ;
-        Character gender2 = gender;
-        System.out.println("My gender is "+ gender2);
+      /*  byte age = 23;
+        Byte byteage = age;
+        byteage = byteage.byteValue();
 
-        String name = "aa";
-        System.out.println("My name is "+ name);
+        short month = 05;
+        Short shortmonth = month;
 
-        float weight = 69.8f;
-        Float weight2 = weight;
-        byte weightbyte = weight2.byteValue();
-        System.out.println("My weight is "+ weightbyte);
+        int day = 15;
+        Integer intday = day;
+        intday = intday.intValue();
 
-        double height = 167.2;
-        Double height2 = height;
-        byte heightbyte = height2.byteValue();
-        System.out.println("My height is " + heightbyte);
+        long year = 1998;
+        Long longyear = year;
+        longyear = longyear.longValue();
+
+        char first = '沈';
+        Character firstname = first;
+        firstname = firstname.charValue();
+
+        String lastname = "俊璋";
+
+        float weight = 64249.61234f;
+        Float floatweight = weight;
+        floatweight = floatweight.floatValue();
+
+        double height = 168.2;
+        Double doubleheight = height;
+        doubleheight = doubleheight.doubleValue();
+
+        boolean a = true;
+        Boolean aa = a;
+        aa = aa.booleanValue();
+
+        boolean b = false;
+        Boolean bb = b;
+        bb = bb.booleanValue();
+
+        System.out.println("My name is "+ firstname + lastname);
+        System.out.println("My age is "+ byteage);
+        System.out.println("My 生日 is "+ longyear + shortmonth + intday);
+        System.out.println("My weight is "+ floatweight);
+        System.out.println("My height is "+ doubleheight);
+        System.out.println("I is 畢業 "+aa);
+        System.out.println("I is 上課 "+bb);*/
 
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -1,14 +1,31 @@
 package com.tom
 
 fun main(args: Array<String>){
-//    println("Hello Kotlin and Java")
-//    Human().hello()
-    val h = Human()
-    h.hello()
+    val h = Human(68.8f, 1.68f)
+    println(h.bmi())
+
+   /* val first = "沈"
+    val last = "俊璋"
+    println("My name is "+first + last)
+
+    var age : Byte
+    age = 23
+    println("My age is "+ age)
+
+    var weight = 69.8f
+    val height = 168
+    println("My weight is "+weight)
+    println("My height is "+ height)*/
+
 }
 
 
-class Human{
+class Human(var weight:Float, var height:Float){
+
+    fun bmi() : Float {
+        val bmi = weight / (height * height)
+        return bmi
+    }
     fun hello(){
         println("Hello Kotlin class")
     }
